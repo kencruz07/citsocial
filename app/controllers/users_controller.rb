9e5980050c2eeb6388f54a929e3b72e7cca606a3
kencruz07 <kencruz07@gmail.com>
2015-07-29 19:44:14 +0800

    Fixed alerts, accepted strings/arrays as alert input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_url, :notice => "Signed up!"
+      flash[:alert] = "Success!"
+      redirect_to root_url
     else
-      render "new"
+      if @user.errors.any?
+        flash[:alert] = @user.errors.full_messages
+        render "new"
+      end
+      
     end
   end
 
