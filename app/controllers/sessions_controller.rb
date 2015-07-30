bce4e7bdd03e16518575daa1fead3d7d61a27396
kencruz07 <kencruz07@gmail.com>
2015-07-30 09:48:02 +0800

    Changed render 'new' to redirect_to root_url, renamed 'main' route to 'index'.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       redirect_to :controller => 'main', :action => 'new'
     else
       flash[:alert] = "Invalid email or password"
-      render "new"
+      redirect_to root_url
     end
   end
 
