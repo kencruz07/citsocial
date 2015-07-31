609b2428e3478dd6b3146123d32456e992672e32
kencruz07 <kencruz07@gmail.com>
2015-07-31 17:40:31 +0800

    Remove () and spaces from posts_controller, edited seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,24 +10,15 @@ MAX_SEEDS = 5
 User.find_each do |user|
   
   number_of_days = 5
-  post_list = []
 
   MAX_SEEDS.times do |post_number|
-    post_list.push({
-      :post_content => "Details about Seed Post #{post_number + 1} goes here", 
-      :post_user_id => user.id,
-      :post_created_at => number_of_days.days.ago,
-      :post_updated_at => number_of_days.days.ago,
-      :post_title => "Seed Post #{post_number + 1}"
-    })
+    Post.create
+        :content => "Details about Seed Post #{post_number + 1} goes here",
+        :user_id => user.id,
+        :created_at => number_of_days.days.ago,
+        :updated_at => number_of_days.days.ago,
+        :title => "Seed Post #{post_number + 1}"
     number_of_days -= 1
   end
-
-  post_list.each do |post|
-    Post.create :content => post[:post_content], :user_id => post[:post_user_id],
-        :created_at => post[:post_created_at], :updated_at => post[:post_updated_at],
-        :title => post[:post_title]
-  end
-
 end
 
