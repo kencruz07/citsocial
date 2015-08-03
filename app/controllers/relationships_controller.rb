d5341761d8f68179e01e0a85f8498df4d7a707c9
kencruz07 <kencruz07@gmail.com>
2015-08-03 20:01:28 +0800

    Created follower/following controller, fixed users/following/follower UI, edited quotes

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,12 @@ class RelationshipsController < ApplicationController
   def create
     user = User.find params[:followed_id]
     current_user.follow user
-    redirect_to users_path
+    redirect_to :back
   end
 
   def destroy
     user = Relationship.find(params[:id]).followed
     current_user.unfollow user
-    redirect_to users_path
+    redirect_to :back
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to posts_path
     else
-      flash[:alert] = "Invalid email or password"
+      flash[:alert] = 'Invalid email or password'
       redirect_to root_url
     end
   end
