6386773a7920908bd2f7c7a62334194606b75f3b
kencruz07 <kencruz07@gmail.com>
2015-08-04 18:40:30 +0800

    Added comments feature, lists comments per post, with user name and comment, added comment count, listed comments by latest comment, improved UI.

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,14 @@
+function hello(){
+  swal({
+    title: 'Are you sure?',
+    text: 'Your will not be able to recover this imaginary file!',
+    type: 'warning',
+    showCancelButton: true,
+    confirmButtonClass: 'btn-danger',
+    confirmButtonText: 'Yes, delete it!',
+    closeOnConfirm: false
+  },
+  function(){
+    swal('Deleted!', 'Your imaginary file has been deleted.', 'success');
+  });
+}
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,44 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-MAX_SEEDS = 5
+#
+MAX_SEEDS = 2
+#
+# User.find_each do |user|
+#
+#   MAX_SEEDS.times do |post_number|
+#     Post.create
+#         :content => "Details about Seed Post #{post_number + 1} goes here",
+#         :user_id => user.id,
+#         :created_at => number_of_days.days.ago,
+#         :updated_at => number_of_days.days.ago,
+#         :title => "Seed Post #{post_number + 1}"
+#     number_of_days -= 1
+#   end
+# end
+#
+Post.find_each do |post|
+  User.find_each do |user|
+    number_of_days = 2
 
-User.find_each do |user|
-  
-  number_of_days = 5
-
-  MAX_SEEDS.times do |post_number|
-    Post.create
-        :content => "Details about Seed Post #{post_number + 1} goes here",
-        :user_id => user.id,
-        :created_at => number_of_days.days.ago,
-        :updated_at => number_of_days.days.ago,
-        :title => "Seed Post #{post_number + 1}"
-    number_of_days -= 1
+    MAX_SEEDS.times do |comment_number|
+      Comment.create :content => "Seed Comment #{comment_number + 1}",
+          :user_id => user.id,
+          :post_id => post.id,
+          :created_at => number_of_days.days.ago,
+          :updated_at => number_of_days.days.ago
+      number_of_days -= 1
+    end
   end
 end
-
+#
+# Post.find_each do |post|
+#   puts post.title
+#   User.find_each do |user|
+#     puts user.first_name
+#     2.times do |comment_number|
+#       puts "comment"
+#     end
+#   end
+# end
+#
