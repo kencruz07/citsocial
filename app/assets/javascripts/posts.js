d52d2c7efd92ec6d2c82507c8236a54ff7ac090e
kencruz07 <kencruz07@gmail.com>
2015-08-05 19:48:32 +0800

    Added comments to post index, added hide/show toggle for comments, added AJAX for posts and comments

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,14 +1,24 @@
 function hello(){
   swal({
     title: 'Are you sure?',
-    text: 'Your will not be able to recover this imaginary file!',
+    text: 'You are about to delete your comment.',
     type: 'warning',
     showCancelButton: true,
     confirmButtonClass: 'btn-danger',
-    confirmButtonText: 'Yes, delete it!',
+    confirmButtonText: 'Delete',
     closeOnConfirm: false
   },
   function(){
-    swal('Deleted!', 'Your imaginary file has been deleted.', 'success');
+    swal('Comment has been deleted!');
   });
+}
+
+function display_comments(id){
+  $('.comment-form-list[data-post-id='+id+']').show();
+  $('.post-panel[data-post-id='+id+']').css('margin-bottom', '-1px');
+}
+
+function hide_comments(id){
+  $('.comment-form-list[data-post-id='+id+']').hide();
+  $('.post-panel[data-post-id='+id+']').css('margin-bottom', '15px');
 }
